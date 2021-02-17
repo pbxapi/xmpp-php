716cb39e2c3a583a4f1ea69519a6cb3de4440150
Aaron Shames <aaron@1pbx.io>
2021-02-17 06:45:48 -0500

    first

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'pbxapi/xmpp-prebind-php',
+  ),
+  'versions' => 
+  array (
+    'pbxapi/xmpp-prebind-php' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
